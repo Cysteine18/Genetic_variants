b462056a667a2e2b0450f157f2e355dd67268853
tarunkhanna1112 <tarun.khanna@imperial.ac.uk>
2018-01-16 23:23:35 +0000

    addtion of c-alpha density functionaliy to strut_effect code

--- structure_influence/struct_effect.py
+++ structure_influence/struct_effect.py
@@ -10,12 +10,14 @@ f.close()
 g = open("del_mut.txt","w")
 g1 = open("ins_mut.txt","w")
 g2 = open("del_ins_mut.txt","w")
+g3 = open("c_alpha_density.txt","w")
 
 # DETERMING THE NUMBER OF DISTINCT CLUSTERS
 
 k = 0
 cid = 1
 nc = 0
+i = 1
 while k < len(ft):
 	ft1 = ft[k].split()
 	if ft1[0] == "#" and ft1[1] == "{}".format(cid):
@@ -35,7 +37,7 @@ while k < len(ft):
 				# DIVIDING THE MUTATIONS INTO THREE SETS OF DELETION, INSERTION AND DELETION-INSERTION MUTATIONS
 				wt1 = wt.split(",")
 				mut1 = mut.split(",")
-				if mut != wt and len(wt1) > 1 and len(mut1) > 1:
+				if len(wt1) > 1 and len(mut1) > 1:
 					wt1 = wt.split(",")
 					mut1 = mut.split(",")
 					k1 = 0
@@ -54,8 +56,9 @@ while k < len(ft):
 							list1.append("{}".format(t1))
 						k1 = k1 + 1
 					if len(mut1) == len(wt1):
-						g1.write("{} {} {} {}".format(nwt,nmut,len(list1),len(wt1)))
-						g1.write("\n")
+						if wt1 != mut1:
+							g1.write("{} {} {} {} {}".format(nwt,nmut,len(list1),len(wt1),len(mut1)))
+							g1.write("\n")
 					else:
 						if len(list1) > 0:
 							g2.write("{} {} {} {} {}".format(nwt,nmut,len(list1),len(wt1),len(mut1)))
@@ -63,6 +66,10 @@ while k < len(ft):
 						else:
 							g.write("{} {} {} {}".format(nwt,nmut,len(wt1),len(mut1)))
 							g.write("\n")
+				if len(wt1) > 1 and len(mut1) > 1:
+					g3.write("{} {} {} {} {}".format(i,nwt,nmut,len(wt1),len(mut1)))
+					g3.write("\n")
+					i = i + 1
 				k = k + 1
 				ft1 = ft[k].split()
 	else:
@@ -71,5 +78,5 @@ while k < len(ft):
 g.close()
 g1.close()
 g2.close()
-
+g3.close()
 
