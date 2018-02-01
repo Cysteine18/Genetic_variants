ace02407bb37e343a77f08a25a27cdde4f4d6bb5
tarunkhanna1112 <tarun.khanna@imperial.ac.uk>
2018-02-01 17:40:45 +0000

    changed script for set2 rmsd cal

--- analysis/sim_struct_sets.py
+++ analysis/sim_struct_sets.py
@@ -6,7 +6,7 @@ def set1():
 
 	import sys
 
-	f = open("{}".format(sys.argv[1]),"r")
+	f = open("{}".format(sys.argv[1]),"r")	# INPUT WHICH CONTAINS ALL THE PAIRS
 	ft = f.readlines()
 	f.close()
 
@@ -28,4 +28,52 @@ def set1():
 
 	g.close()
 
-set1()
+def set2():
+	
+	# SETS OF PDB CHAINS FROM DIFFERENT PDB's BUT SAME AUTHORS
+
+	import sys
+
+	f = open("{}".format(sys.argv[1]),"r")
+	ft = f.readlines()
+	f.close()
+
+	h = open("author.idx","r")		# CONTAINING THE AUTHOR LIST
+	ht = h.readlines()
+	h.close()
+
+	# FORMING A DICTIONARY OF AUTHORS
+
+	d = dict()
+	k = 5
+	while k < (len(ht)-1):
+		ht1 = ht[k].split(" ; ")
+		t1 = ht1[1].split(",")
+		aut = t1[0]
+		pdb = ht1[0]
+
+		d["{}".format(pdb)] = aut
+
+	g = open("{}".format(sys.argv[2]),"w")
+
+	k = 0
+	while k < len(ft):
+		print("{} of {}".format(k,len(ft)))
+		ft1 = ft[k].split()
+		t1 = ft1[0].strip("\n")
+		t2 = ft1[1].strip("\n")
+		t3 = ft1[2].strip("\n")
+		t4 = ft1[3].strip("\n")
+
+		t1u = t1.upper()
+		t3u = t3.upper()
+
+		if d["{}".format(t1u)] == d["{}".format(t3u)]:
+			g.write("{} {} {} {}".format(t1,t2,t3,t4))
+			g.write("\n")
+
+		k = k + 1
+
+	h1.close()
+	
+set2()
