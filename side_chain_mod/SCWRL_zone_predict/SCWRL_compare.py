638b4ed524e632dc30af72f020a29c0f269a5325
tarun.khanna@imperial.ac.uk <tarun.khanna@imperial.ac.uk>
2018-03-28 00:51:55 +0100

    change in SCWRL compare code

--- side_chain_mod/SCWRL_zone_predict/SCWRL_compare.py
+++ side_chain_mod/SCWRL_zone_predict/SCWRL_compare.py
@@ -1,17 +1,16 @@
 import sys
+import math
 
 f = open("{}".format(sys.argv[1]),"r")		# RMSD SCWRL
 ft = f.readlines()
 f.close()
 
-g = open("{}".format(sys.argv[2]),"r")		# RMSD CRYSTAL STRUCTURE
-gt = g.readlines()
-g.close()
-
 h = open("SCWRL_compare.csv","w")
-h.write("#CRY_MUT,CHAIN,PRED_MUT,CHAIN,LRMSD_SC(0),LRMSD_SC(4),LRMSD_SC(5),LRMSD_SC(6), LRMSD_SC(7),LRMSD_SC(8),LRMSD_SC(9),LRMSD_SC(10),LRMSD_SC(crystal),LRMSD\n")
+h.write("#CRY_MUT,CHAIN,PRED_MUT,CHAIN,LRMSD_SC(0),LRMSD_SC(4),LRMSD_SC(5),LRMSD_SC(6), LRMSD_SC(7),LRMSD_SC(8),LRMSD_SC(9),LRMSD_SC(10)\n")
 
-k = 1
+h1 = open("mean_value.txt","w")
+d = dict()
+k = 0
 while k < len(ft):
 	print("{} of {}".format(k,len(ft)))
 	ft1 = ft[k].split(",")
@@ -28,41 +27,47 @@ while k < len(ft):
 	wtc1 = temp[0][4:(len(temp[0]))]
 	mut1 = temp[1][0:4]
 	mutc1 = temp[1][4:(len(temp[0]))]
-	k1 = 0
-	gt1 = gt[k1].split(",")
-	t2 = gt1[0]
-	t3 = gt1[1]
-	t4 = gt1[2]
-	t5 = gt1[3]
 
-	if lenwt == allen and lenmut == allen:
-		count = 0
-		while t2 != wt1 or t3 != wtc1 or t4 != mut1 or t5 != mutc1:
-			k1 = k1 + 1
-			if k1 >= len(gt):
-				count = count + 1
-				break
-			gt1 = gt[k1].split(",")
-			t2 = gt1[0]
-			t3 = gt1[1]
-			t4 = gt1[2]
-			t5 = gt1[3]
-
-		if count == 0:
-			lrmsdsc = gt1[9].strip("\n")
-			lrmsd = gt1[8]
-
-			if lrmsdsc != "NA":
-				list1 = []
-				for x in range(0,8):
-					ft1 = ft[(k+x)].split(",")
-					t1 = ft1[9]
-					if t1 != "NA":
-						list1.append(t1)
-				if len(list1) == 8:
-					h.write("{},{},{}-{},{},{},{},{},{},{},{},{},{},{},{}\n".format(wt,wtc,wt1,mut1,mutc,list1[0],list1[1],list1[2],list1[3],list1[4],list1[5], list1[6],list1[7],lrmsdsc,lrmsd))
+	list1 = []
+	for x in range(0,8):
+		ft1 = ft[(k+x)].split(",")
+		t1 = ft1[9]
+		if t1 != "NA":
+			if x in d.keys():
+				d[x].append(t1)
+			else:
+				d[x] = [t1]
+			list1.append(t1)
+		if len(list1) == 8:
+			h.write("{},{},{}-{},{},{},{},{},{},{},{},{},{}\n".format(wt,wtc,wt1,mut1,mutc,list1[0],list1[1],list1[2],list1[3],list1[4],list1[5], list1[6],list1[7]))
 	
 	k = k + 8
+	
 
+# MEAN FOR EACH ZONE
+
+list1 = [1,4,5,6,7,8,9,10]
+for x in range(0,8):
+	k = 0
+	mean = 0.0
+	while k < len(d[x]):
+		t1 = float(d[x][k])
+		mean = mean + t1
+		k = k + 1
+	mean = mean / len(d[x])
+	SD = 0.0
+	k = 0
+	while k < len(d[x]):
+		t1 = float(d[x][k])
+		SD = t1 - mean
+		SD = SD * SD
+		k = k + 1
+	SD = SD / len(d[x])
+	SD = math.sqrt(SD)
+	mean = round(mean,2)
+	SD = round(SD,2)
+	h1.write("{}	{}	{}\n".format(list1[x],mean,SD))
+
+h1.close()
 h.close()
 
